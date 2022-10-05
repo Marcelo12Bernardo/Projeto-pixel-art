ad245c98f54e337fa2bcab18a11a5caa574fc8a1
Marcelo Bernardo <bernardomarcelo33@gmail.com>
2022-10-05 12:45:26 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -1,25 +1,58 @@
 window.onload = function () {
     //Elementos da paleta
-    const colorOne = document.getElementById('colorOne');
+    //const colorOne = document.getElementById('colorOne');
     const colorTwo = document.getElementById('colorTwo');
     const colorTree = document.getElementById('colorTree');
     const colorFor = document.getElementById('colorFor');
+    
     //Botao
     const btnSortColor = document.querySelector('#button-random-color');
-    //Seta cores aleatorias para a paleta
+
+    //Carrega cores
+    loadColor();
+
+    // Gera cores aleatorias para a paleta // Click btn
     btnSortColor.addEventListener('click', function () {
-        colorTwo.style.backgroundColor = colorRandom();
-        colorTree.style.backgroundColor = colorRandom();
-        colorFor.style.backgroundColor = colorRandom();
+        let paleta = colorRandom();
+        setColor(paleta);
+        saveColor(paleta);
     });
+
+    //Seta  a cor de fundo dos elemntos
+    function setColor(paletaDeCores){
+        colorTwo.style.backgroundColor = paletaDeCores[0];
+        colorTree.style.backgroundColor = paletaDeCores[1];
+        colorFor.style.backgroundColor = paletaDeCores[2];
+    }
     //Gera cores Hexa aleatorias
     function colorRandom(){
         const parts = '0123456789ABCDEF';
-        let color = '#';
-        for (let cont = 0; cont < 6; cont++) {
-        color += parts[Math.floor(Math.random() * 16)];
+        let paleta = [];
+        for (let bloc = 0; bloc < 3; bloc++) {
+            let cor = '#';
+            for (let cont = 0; cont < 6; cont++) {
+                cor += parts[Math.floor(Math.random() * 16)];
+            }
+            paleta[bloc] = cor;
         }
-        return color; 
+        return paleta; 
     }
+    
+    //Salva as cores da paleta
+    function saveColor(palet) {
+        localStorage.setItem('colorPalette',JSON.stringify(palet));
+    }
+    
+    //Carrega as cores da paleta
+    function loadColor() {
+        if(localStorage.length === 0){
+            let coresUsadas = ['#ff0000','#ffff00','#0000ff'];
+            setColor(coresUsadas);
+        }else{
+            let coresUsadas = JSON.parse(localStorage.getItem("colorPalette"))
+            setColor(coresUsadas);
+        }
+    }
+
 }
 //Nota: Hexa vai de 0-9 e A-F e tem 6 partes
